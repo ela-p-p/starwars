8ffb6c26bead7aa7bc2b21be437c0609c7523415
ela-p-p <elizabeth.porter.pevec@gmail.com>
2020-01-08 15:23:54 -0500

    Updates

---
+++ precache-manifest.b7fe3c95d0fa83b411ec6c82cf7517ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d1528dc47c0d8e4c584918a248adde",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "71e10c92c8cfa0982a03",
+    "url": "/starwars/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "96775983e0b56aa2a5e4",
+    "url": "/starwars/static/css/main.7ac7fd62.chunk.css"
+  },
+  {
+    "revision": "71e10c92c8cfa0982a03",
+    "url": "/starwars/static/js/2.a83607a9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/starwars/static/js/2.a83607a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "96775983e0b56aa2a5e4",
+    "url": "/starwars/static/js/main.e1accb29.chunk.js"
+  },
+  {
+    "revision": "9991ba5ad118bff061a1",
+    "url": "/starwars/static/js/runtime-main.344ed7db.js"
+  },
+  {
+    "revision": "c60bda4169a051d9a43e96934acddd9a",
+    "url": "/starwars/static/media/starwars.c60bda41.jpg"
+  }
+]);
